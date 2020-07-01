640f8be8833652457408cb68d3d56db4410f5d69
root <root@localhost.localdomain>
2020-07-01 21:38:27 +0200

    Version Final :)

--- java.js
+++ java.js
@@ -4,7 +4,20 @@ let canavas = document.getElementById("game")
 let context = canavas.getContext("2d")
 
 
-import Life from  "live.js"
+import Life from "./life.js"
+
+                      //Canvas   H    L   Scale  Time
+let lifeGame = new Life(context, 200, 100, 30, 1)
 
 
-let game = new Life(context, 100, 100, 10, 10)
+/*
+H   ==>  Hauteur du tableaux
+L   ==>  Largeur du tableaux
+Scale ==> Grandeur de chaque cellule
+Time ==> Time de refresh
+
+
+console.log("Hey :)")
+console.log("Voici les régles: ")
+console.log()
+*/
--- life.js
+++ life.js
@@ -1,87 +1,112 @@
 
+/*
+
+1 - Toute cellule vivante avec moins de deux voisins vivants meurt, comme par sous-population.
+2 - Toute cellule vivante avec deux ou trois voisins vivants vit jusqu'à la prochaine génération.
+3 - Toute cellule vivante avec plus de trois voisins vivants meurt, comme par surpopulation.
+4 - Toute cellule morte avec exactement trois voisins vivants devient une cellule vivante, comme par reproduction.
+
+
+
+*/
+
+
 
 class Life{
   constructor(canvas, weigth, heigth, scale, refresh){
+    this.canvas = canvas
     this.game = []
-    this.canavas = canavas
-    this.init()
     this.weigth = weigth
     this.heigth = heigth
     this.refresh = refresh
     this.scale = scale
+
+    this.init()
   }
 
 
   init(){
     for(let y = 0 ; y < this.heigth ; y++){
+      this.game[y] = []
       for(let x = 0 ; x < this.weigth ; x++){
         this.game[y][x] = Math.floor(Math.random() * 2)
       }
     }
+
+    this.write(this.game)
     this.loop()
   }
-}
 
 
-write(array){
-  for(let y = 0 ; y < array.length ; y++){
-    for(let x = 0 ; x < array[y].length ; x++){
-      if(array[y][x] === 1){
-        this.canvas.fillStyle = "black"
+
+  write(array){
+    for(let y = 0 ; y < array.length ; y++){
+      for(let x = 0 ; x < array[y].length ; x++){
+        if(array[y][x] === 1){
+          this.canvas.fillStyle = "#ecf0f1" //2c3e50
+        }
+        else if (array[y][x] === 0) {
+          this.canvas.fillStyle = "#2c3e50"  //ecf0f1
+        }
+        let reactX = ((x+1) * this.scale) - this.scale
+        let reactY = ((y+1) * this.scale) - this.scale
+        this.canvas.fillRect(reactX, reactY, this.scale, this.scale)
       }
-      else if (array[y][x] === 0) {
-        this.canvas.fillSytle = "white"
+    }
+  }
+
+
+  checklife(){
+    let newGame = []
+    for(let y = 0 ; y < this.heigth ; y++){
+      newGame[y] = []
+      for(let x = 0 ; x < this.weigth ; x++){
+          newGame[y][x] = 0
       }
-      let reactX = (y * this.scale) - this.scale
-      let reactY = (y * this.scale) - this.scale
-      this.canavas.fillRect(x, y, this.scale, this.scale)
     }
-  }
-}
 
 
-checklife(){
-  for(let y = 0 ; y < this.heigth ; y++){
-    for(let x = 0 ; x < this.weigth ; x++){
-        this.game = life(y, x, this.game)
+    for(let y = 0 ; y < this.heigth ; y++){
+      for(let x = 0 ; x < this.weigth ; x++){
+        let live = this.life(y, x, this.game)
 
+        if(this.game[y][x] === 0 && live === 3){
+          newGame[y][x] = 1
+        }
+        else if(this.game[y][x] === 1 && (live < 2 || live > 3) ){
+          newGame[y][x] = 0
+        }
+        else if(this.game[y][x] === 1 && (live === 2 || live === 3) ){
+          newGame[y][x] = this.game[y][x]
+        }
+      }
     }
+    this.game = newGame
+    this.write(newGame)
   }
-  write(this.game)
-}
 
-life(currentY, currentX, array){
 
-  let newGame = []
-  let num = 0
-  for(let y = -1; y < 2 ; y++){
-    for(let x = -1; x < 2; x++){
-      let row = (x + currentY ) % this.weigth
-      let col = (y + currentX )
-      num += this.array[col][row]
+  life(currentY, currentX, array){
+    let live = 0
+    for(let y = -1; y < 2 ; y++){
+      for(let x = -1; x < 2; x++){
+        let row = (currentX + x + this.weigth) % this.weigth
+        let col = (currentY + y + this.heigth) % this.heigth
+        live += array[col][row]
+      }
     }
+    live -= array[currentY][currentX]
+
+    return live
   }
-  num -= this.array[currenty][currentX]
 
-  if(this.array[currentY][currentX] === 0 && num === 3){
-    newGame[currenY][currentX] = 1
+
+
+  loop(){
+    setInterval(()=>{
+      this.checklife()
+    }, this.refresh)
   }
-  else if(this.array[currentY][currentX] === 0 && (num < 2 || num > 3) ){
-      newGame[currenY][currentX] = 0
-  }
-  else{
-    newGame[currenY][currentX] = 0
-  }
-  return newGame
-
-}
-
-
-
-loop(){
-  setInterval(()=>{
-    checklife()
-  }, refresh)
 }
 
 
